33d2cc98d0f0ee1f3750411aa8549bdfbe0b9b5e
SaurabhOllato <saurabh.ollato@gmail.com>
2025-06-24 16:53:00 +0530

    vercel deployment 3

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 import AdminLayout from "./layouts/AdminLayout";
@@ -7,22 +12,25 @@ import AddProduct from "./pages/admin/AddProduct";
 import ManageProducts from "./pages/admin/ManageProducts";
 import WhatsAppFloating from "./pages/Whatsapp";
 import ManageOrders from "./pages/admin/ManageOrders";
-import './index.css';
+import "./index.css";
 
 const isAdmin = true;
 
 const App = () => {
   return (
-    <Router  basename="/">
-      <div id="scroll-detector" style={{ position: "absolute", top: 0, height: "1px", width: "100%" }}></div>
+    <Router>
+      <div
+        id="scroll-detector"
+        style={{ position: "absolute", top: 0, height: "1px", width: "100%" }}
+      ></div>
       <div className="w-full bg-black overflow-hidden">
-  {/* <div className="whitespace-nowrap animate-marquee text-white text-sm py-2">
+        {/* <div className="whitespace-nowrap animate-marquee text-white text-sm py-2">
     <span className="mx-8">🔥 20% off on all earrings</span>
     <span className="mx-8">💎 Free shipping above ₹999</span>
     <span className="mx-8">✨ Buy 2 Get 1 Free on Bangles</span>
     <span className="mx-8">🛍️ New Collection Just Dropped!</span>
   </div> */}
-</div>
+      </div>
       <Navbar />
       <Routes>
         {/* Normal app routes */}
